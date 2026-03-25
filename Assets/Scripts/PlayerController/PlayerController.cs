b26ebf4b3cc2db56d8f426a56d132d8f574f6cdc
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-03-24 21:03:58 -0600

    Changed to physics based movemnet

--- Assets/Scripts/PlayerController/PlayerController.cs
+++ Assets/Scripts/PlayerController/PlayerController.cs
@@ -5,13 +5,11 @@ using UnityUtils;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Dependencies")]
+    [Header("References")]
     [SerializeField] private InputReader input;
-    [SerializeField] private Transform _transform; 
-    
-    [Header("Physics")]
+    [SerializeField] private Transform _transform;
+    [SerializeField] private Rigidbody2D rb; 
     [SerializeField] private BoxCollider2D col;
-    [SerializeField] private LayerMask wallLayer;
 
     [Header("Settings")]
     [SerializeField] private FloatVariable movementSpeed;
@@ -23,12 +21,7 @@ public class PlayerController : MonoBehaviour
 
     private void HandleMovement()
     {
-        Vector2 moveDir = input.Direction;
-        if (IsPositionInWall(_transform.position + moveDir.ToVector3())) return; 
-        
-        transform.Translate( movementSpeed.Value * Time.deltaTime * moveDir);
+        Vector2 moveDir = input.Direction.normalized;
+        rb.linearVelocity = moveDir * movementSpeed.Value;
     }
-
-    private bool IsPositionInWall(Vector2 pos) =>
-        Physics2D.OverlapBox(pos, col.size, 0f, wallLayer); 
 }
