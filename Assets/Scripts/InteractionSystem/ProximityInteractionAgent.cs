193cd986f9ddb24b69f44ec48457d6418041a5d5
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-03-24 21:58:03 -0600

    It works!

---
+++ Assets/Scripts/InteractionSystem/ProximityInteractionAgent.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Registry;
+
+namespace InteractionSystem
+{
+    public class ProximityInteractionAgent : MonoBehaviour
+    {
+        [SerializeField] private Transform _transform; 
+        [SerializeField] private float interactionRange = 2f;
+        [SerializeField] private InputReader input;
+        [SerializeField] private GameObject interactionIndicator; 
+        private IInteractable closestTarget; 
+        
+        private void Awake()
+        {
+            input.onInteract += TryInteract;
+            
+            interactionIndicator.SetActive(false);
+            interactionIndicator.transform.SetParent(null); 
+        }
+        
+        private void TryInteract(bool wasPressed)
+        {
+            print(wasPressed);
+            if (wasPressed) closestTarget?.Interact();
+        }
+        
+        private void Update()
+        {
+            closestTarget = Registry<IInteractable>.Get(new Closest(interactionRange, _transform.position));
+            if (closestTarget == null)
+            {
+                interactionIndicator.SetActive(false);
+                return; 
+            }
+            
+            if (closestTarget is Component component)
+            {
+                interactionIndicator.SetActive(true);
+                interactionIndicator.transform.position = component.transform.position;
+            }
+            else
+                interactionIndicator.SetActive(false);
+        }
+
+        
+        private void OnDestroy()
+        {
+            input.onInteract -= TryInteract; 
+        }
+    }
+}
