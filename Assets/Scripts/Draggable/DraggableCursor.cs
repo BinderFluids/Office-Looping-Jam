27a0db7dedc4c2c66be7a27a502d64c9ade796d6
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-03-26 03:12:39 -0600

    Update DraggableCursor.cs

--- Assets/Scripts/Draggable/DraggableCursor.cs
+++ Assets/Scripts/Draggable/DraggableCursor.cs
@@ -1,6 +1,7 @@
 using System;
 using UnityEngine;
 using Registry;
+using UnityUtils;
 
 namespace Draggable
 {
@@ -8,12 +9,30 @@ namespace Draggable
     {
         [SerializeField] private InputReader input; 
         [SerializeField] private float cursorSize = 10f;
+
+        private Vector2 offset; 
+        private DraggableBehaviour target;
         
         private void Update()
         {
-            DraggableBehaviour target =
-                Registry<DraggableBehaviour>
-                    .Get(new Closest(cursorSize, input.MouseWorldPosition));
+            if (input.Click.WasPressedThisFrame)
+            {
+                target =
+                    Registry<DraggableBehaviour>
+                        .Get(new Closest(cursorSize, input.MouseWorldPosition));
+
+                if (target != null)
+                {
+                    offset = target.transform.position - input.MouseWorldPosition.ToVector3();
+                }
+            }
+
+            if (target != null)
+                target.transform.position = input.MouseWorldPosition + offset; 
+
+            
+            if (!input.Click.IsPressed)
+                target = null;
         }
     }
 }
