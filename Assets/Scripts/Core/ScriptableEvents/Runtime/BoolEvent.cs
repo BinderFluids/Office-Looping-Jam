00dfdf6b04a125e4a7f930437bc4d6e3556f56a6
Tanner Martin <79233634+BinderFluids@users.noreply.github.com>
2026-04-06 16:46:35 -0600

    Add bool event and listener

---
+++ Assets/Scripts/Core/ScriptableEvents/Runtime/BoolEvent.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable Events/Bool Event")]
+public class BoolEvent : ScriptableEvent<bool> { }
---
+++ Assets/Scripts/Core/ScriptableEvents/Runtime/BoolEventListener.cs
@@ -0,0 +1,1 @@
+public class BoolEventListener : GameEventListener<bool> { }
